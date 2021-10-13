c546b7d6ab32fd28384c36647c31ddf9ae25f242
849961 <849961@SAIT227700IT.ACDM.DS.SAIT.CA>
2021-10-13 11:38:25 -0600

    edited note.java file

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -10,5 +10,27 @@ package models;
  * @author 849961
  */
 public class Note {
+    private String title;
+    private String content;
     
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public String getContent(){
+        return content;
+    }
+    
+    public void setContent(String content){
+        this.content = content;
+    }
 }
